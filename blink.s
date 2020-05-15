2c540e8fc06218239d21c0a89d2466737f4ecfc0
Jacob Krall <jacob@jkrall.net>
2020-05-14 23:03:27 -0400

    use the assembler to calculate addresses

--- blink.s
+++ blink.s
@@ -1,15 +1,17 @@
   .org $8000
+reset:
   lda #$ff
   sta $6002
 
+loop:
   lda #$55
   sta $6000
 
   lda #$aa
   sta $6000
 
-  jmp $8005
+  jmp loop
 
   .org $fffc
-  .word $8000
+  .word reset
   .word $0000
