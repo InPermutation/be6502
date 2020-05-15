cd5138943d86e40a34946ec2c75e21371a170b55
Jacob Krall <jacob@jkrall.net>
2020-05-15 18:45:44 -0400

    start talking to HD44780U

--- blink.s
+++ blink.s
@@ -1,14 +1,68 @@
+PORTB = $6000
+PORTA = $6001
+DDRB  = $6002
+DDRA  = $6003
+
+E = %10000000
+RW = %01000000
+RS = %00100000
+
   .org $8000
 reset:
-  lda #$ff
-  sta $6002
+  lda #%11111111 ; Set all pins on port B to output
+  sta DDRB
 
-  lda #$50
-  sta $6000
+  lda #%11100000 ; Set top 3 pins on port A to output
+  sta DDRA
+
+; Set mode
+  lda #%00111000 ; Set 8-bit mode; 2-line display 5x8 font
+  sta PORTB
+
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+
+  lda #E         ; Set E bit to enable display
+  sta PORTA
+
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+
+; Display on/off
+  lda #%00001110 ; Display on; cursor on ; blink off
+  sta PORTB
+
+  lda #E         ; Set E bit to enable display
+  sta PORTA
+
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+
+; Entry mode set
+  lda #%00000110 ; I/D: move direction inc/dec, S: display shift?
+  sta PORTB
+
+  lda #E         ; Set E bit to enable display
+  sta PORTA
+
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+
+; Write data:
+  lda #"H"
+  sta PORTB
+
+  lda #RS        ; Set RS
+  sta PORTA
+
+  lda #(RS | E)  ; Send data to display
+  sta PORTA
+
+  lda #RS         ; Clear RS/RW/E bits
+  sta PORTA
+
 
 loop:
-  ror
-  sta $6000
   jmp loop
 
   .org $fffc
