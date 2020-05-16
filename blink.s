0b6251badab3b5a8637c0a9874b090cfdd4548b6
Jacob Krall <jacob@jkrall.net>
2020-05-16 18:15:28 -0400

    Use stack to implement subroutines

--- blink.s
+++ blink.s
@@ -9,6 +9,10 @@ RS = %00100000
 
   .org $8000
 reset:
+  ; initialize stack pointer to $01FF
+  ldx #$ff
+  txs
+
   lda #%11111111 ; Set all pins on port B to output
   sta DDRB
 
@@ -17,63 +21,50 @@ reset:
 
 ; Set mode
   lda #%00111000 ; Set 8-bit mode; 2-line display 5x8 font
-  sta PORTB
-
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
-
-  lda #E         ; Set E bit to enable display
-  sta PORTA
-
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
+  jsr lcd_instruction
 
 ; Display on/off
   lda #%00001110 ; Display on; cursor on ; blink off
-  sta PORTB
-
-  lda #E         ; Set E bit to enable display
-  sta PORTA
-
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
+  jsr lcd_instruction
 
 ; Entry mode set
   lda #%00000110 ; I/D: move direction inc/dec, S: display shift?
-  sta PORTB
-
-  lda #E         ; Set E bit to enable display
-  sta PORTA
-
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
+  jsr lcd_instruction
 
   ldx #0
 
 write_str:
   lda str,x
-  sta PORTB
-
   beq loop       ; exit loop if str[x] is zero
-
-  lda #RS        ; Set RS
-  sta PORTA
-
-  lda #(RS | E)  ; Send data to display
-  sta PORTA
-
-  lda #RS         ; Clear E bit
-  sta PORTA
-
+  jsr print_char
   inx
   jmp write_str
-
 
 loop:
   jmp loop
 
+lcd_instruction:
+  sta PORTB
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  lda #E         ; Set E bit to enable display
+  sta PORTA
+  lda #0         ; Clear RS/RW/E bits
+  sta PORTA
+  rts
+
+print_char:
+  sta PORTB
+  lda #RS        ; Set RS
+  sta PORTA
+  lda #(RS | E)  ; Send data to display
+  sta PORTA
+  lda #RS         ; Clear E bit
+  sta PORTA
+  rts
+
 str:
-  .asciiz "Hello, loop!"
+  .asciiz "Hello, RAM!"
 
   .org $fffc
   .word reset
