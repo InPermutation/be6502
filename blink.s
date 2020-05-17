d74d48e09f2fc4d1eed9312e5f3b5c5543a3364c
Jacob Krall <jacob@jkrall.net>
2020-05-17 15:42:48 -0400

    Key matrix
    I don't have any diodes, so this matrix has bad ghosting on
    L+R, D+B, U+A. If I had some diodes I could fix that. I could also
    eliminate one of the buttons and just use the 5 available lines on
    port A directly.
    
    This program also doesn't do any debouncing (nor does the physical
    keyboard) so it's better for continuous motion (moving Mario around,
    turning a spaceship, turning the light on/off, etc.) where being
    pulsed rapidly won't have a bad effect. Not great for typing, though!

--- blink.s
+++ blink.s
@@ -3,8 +3,13 @@ PORTA = $6001
 DDRB  = $6002
 DDRA  = $6003
 DDRA_NORMAL = %11100000
+DDRA_LOW_PA4 = %11110000
+DDRA_LOW_PA3 = %11101000
 ALL_IN = %00000000
 ALL_OUT = %11111111
+
+LDU = $00 ; Zero page baby
+RBA = $01
 
 E = %10000000
 RW = %01000000
@@ -48,6 +53,105 @@ write_str:
   jmp write_str
 
 loop:
+  ; Let's make a D-pad
+  ;    U
+  ;  L   R     B   A
+  ;    D
+  ;
+  ; Wired in a 2x3 matrix as follows:
+  ;      PA3  PA4
+  ; PA0   L    R
+  ; PA1   D    B
+  ; PA2   U    A
+  ; PA0-2 are pulled high by 1k resistors.
+  ; This means PA3-4 are the driving pins and PA0-2 are for reading.
+
+  ; Set PA3 tristate (read) and PA4 low
+  lda #DDRA_LOW_PA4
+  sta DDRA
+
+  ; Then read PA0-2
+  lda #0
+  sta PORTA
+  lda PORTA
+  and #%00000111 ; only care about the bottom 3 bits
+  sta RBA
+
+  ; Set PA3 low and PA4 tristate (read)
+  lda #DDRA_LOW_PA3
+  sta DDRA
+
+  ; Then read PA0-2 again
+  lda #0
+  sta PORTA
+  lda PORTA
+  and #%00000111 ; only care about the bottom 3 bits
+  sta LDU
+
+  ; Restore DDRA
+  lda #DDRA_NORMAL
+  sta DDRA
+
+  lda #%00000010 ; Reset home
+  jsr lcd_instruction
+
+  ldx #" "
+  lda LDU
+  and #%00000001
+  bne p_l
+  ldx #"L"
+p_l:
+  txa
+  jsr print_char
+
+  ldx #" "
+  lda LDU
+  and #%00000010
+  bne p_d
+  ldx #"D"
+p_d:
+  txa
+  jsr print_char
+
+  ldx #" "
+  lda LDU
+  and #%00000100
+  bne p_u
+  ldx #"U"
+p_u:
+  txa
+  jsr print_char
+
+  lda #"_"
+  jsr print_char
+
+  ldx #" "
+  lda RBA
+  and #%00000001
+  bne p_r
+  ldx #"R"
+p_r:
+  txa
+  jsr print_char
+
+  ldx #" "
+  lda RBA
+  and #%00000010
+  bne p_b
+  ldx #"B"
+p_b:
+  txa
+  jsr print_char
+
+  ldx #" "
+  lda RBA
+  and #%00000100
+  bne p_a
+  ldx #"A"
+p_a:
+  txa
+  jsr print_char
+
   jmp loop
 
 lcd_instruction:
