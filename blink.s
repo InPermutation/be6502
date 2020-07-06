cc4a24f1976978e16823ddbc6a866643c7698ae3
Jacob Krall <jacob@jkrall.net>
2020-07-06 15:49:14 -0400

    Get rid of game controller; switch to 4x20 LCD

--- blink.s
+++ blink.s
@@ -53,105 +53,6 @@ write_str:
   jmp write_str
 
 loop:
-  ; Let's make a D-pad
-  ;    U
-  ;  L   R     B   A
-  ;    D
-  ;
-  ; Wired in a 2x3 matrix as follows:
-  ;      PA3  PA4
-  ; PA0   L    R
-  ; PA1   D    B
-  ; PA2   U    A
-  ; PA0-2 are pulled high by 1k resistors.
-  ; This means PA3-4 are the driving pins and PA0-2 are for reading.
-
-  ; Set PA3 tristate (read) and PA4 low
-  lda #DDRA_LOW_PA4
-  sta DDRA
-
-  ; Then read PA0-2
-  lda #0
-  sta PORTA
-  lda PORTA
-  and #%00000111 ; only care about the bottom 3 bits
-  sta RBA
-
-  ; Set PA3 low and PA4 tristate (read)
-  lda #DDRA_LOW_PA3
-  sta DDRA
-
-  ; Then read PA0-2 again
-  lda #0
-  sta PORTA
-  lda PORTA
-  and #%00000111 ; only care about the bottom 3 bits
-  sta LDU
-
-  ; Restore DDRA
-  lda #DDRA_NORMAL
-  sta DDRA
-
-  lda #%00000010 ; Reset home
-  jsr lcd_instruction
-
-  ldx #" "
-  lda LDU
-  and #%00000001
-  bne p_l
-  ldx #"L"
-p_l:
-  txa
-  jsr print_char
-
-  ldx #" "
-  lda LDU
-  and #%00000010
-  bne p_d
-  ldx #"D"
-p_d:
-  txa
-  jsr print_char
-
-  ldx #" "
-  lda LDU
-  and #%00000100
-  bne p_u
-  ldx #"U"
-p_u:
-  txa
-  jsr print_char
-
-  lda #"_"
-  jsr print_char
-
-  ldx #" "
-  lda RBA
-  and #%00000001
-  bne p_r
-  ldx #"R"
-p_r:
-  txa
-  jsr print_char
-
-  ldx #" "
-  lda RBA
-  and #%00000010
-  bne p_b
-  ldx #"B"
-p_b:
-  txa
-  jsr print_char
-
-  ldx #" "
-  lda RBA
-  and #%00000100
-  bne p_a
-  ldx #"A"
-p_a:
-  txa
-  jsr print_char
-
   jmp loop
 
 lcd_instruction:
@@ -202,10 +103,17 @@ lcd_wait_loop:
 
 
 str:
-  .asciiz "Hello from 6502!                        jacob.jkrall.net"
+  .asciiz "Hello from 6502!   1Maybe line 3?      3jacob.jkrall.net   2Guessing line 4?   4"
   ;        12345678901234567890123456789012345678901234567890123456789012345678901234567890
   ;                        xxxxxxxxxxxxxxxxxxxxxxxx                xxxxxxxxxxxxxxxxxxxxxxxx
 
-  .org $fffc
+irq_brk:
+  rti
+
+nmi:
+  rti
+
+  .org $fffa
+  .word nmi
   .word reset
-  .word $0000
+  .word irq_brk
