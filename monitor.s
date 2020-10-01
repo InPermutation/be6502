5345893fb389e6c18c2f99c6ddd56dcd950a5dbf
Jacob Krall <jacob@jkrall.net>
2020-09-30 20:21:24 -0400

    monitor_read_word: greedily consume hex characters into .word MONITOR

--- monitor.s
+++ monitor.s
@@ -1,5 +1,5 @@
 ; zero page addresses
-MONITOR_LO = $20 ; store an address here
+MONITOR = $20 ; store a word read from TTY_READLINE here
 MONITOR_HI = $21
 MONITOR_DATA = $22
 MONITOR_DATA_HI = $23
@@ -10,14 +10,49 @@ monitor:
   puts s_reset_3
 monitor_loop:
   jsr readline
-  lda TTY_READLINE
+  stz TTY_READLINE
+  jsr monitor_read_word
+  lda MONITOR_HI
   jsr print_hex_byte
-  puts s_ok
-
+  lda MONITOR
+  jsr print_hex_byte
+  jsr tty_scroll
   jmp monitor_loop
 
+monitor_read_word:
+  pha
+  phx
+  stz MONITOR
+  stz MONITOR_HI
+_read_next_char:
+  lda (TTY_READLINE)
+  ldx #0
+_hex_loop:
+  cpx #$10
+  beq _nomatch
+  cmp s_hex,x
+  beq _match
+  inx
+  jmp _hex_loop
+_match:
+_shift_nibble:
+  .rept 4
+  asl MONITOR
+  rol MONITOR_HI
+  .endr
+  txa
+  clc
+  adc MONITOR
+  sta MONITOR
+  inc TTY_READLINE
+  jmp _read_next_char
+_nomatch:
+  plx
+  pla
+  rts
+
+  .align 8
 s_reset: .asciiz "0000<CR>->inspect"
 s_reset_2: .asciiz "0000:12<CR>->set"
 s_reset_3: .asciiz "0000R<CR>->run"
-s_ok: .asciiz " chars read."
 
